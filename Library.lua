e2b02e59dd46dc384c522f5744f66fb5282a15ae
goatgamer99 <6uggedsalt@proton.me>
2025-04-27 03:07:24 -0400

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -35,7 +35,7 @@ local Library = {
     OutlineColor = Color3.fromRGB(28, 28, 28);
     RiskColor = Color3.fromRGB(255, 50, 50),
 
-    Black = Color3.new(0, 0, 0);
+    Black = Color3.new(255, 130, 150);
     Font = Enum.Font.SciFi,
 
     OpenedFrames = {};
